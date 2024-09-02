c39e18f5285b0aad8878c7123d2360869366548f
Kyle Le <158103657+Kyle-Le-create@users.noreply.github.com>
2024-09-02 12:03:30 -0500

    New update for my project

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -72,26 +72,47 @@ function App() {
           <input
             type="Url"
             className="modal__input"
-            id="imageURL"
+            id="imageUrl"
             placeholder="Image URL"
           />
         </label>
         <fieldset className="modal__radio-buttons">
           <legend className="modal__legend">Select the weather type:</legend>
           <label htmlFor="hot" className="modal__label modal__label_type_radio">
-            <input id="hot" type="radio" className="modal__radio-input" /> Hot
+            <input
+              id="hot"
+              type="radio"
+              className="modal__radio-input"
+              name="weatherType"
+              value="Hot"
+            />
+            Hot
           </label>
           <label
             htmlFor="warm"
             className="modal__label modal__label_type_radio"
           >
-            <input id="warm" type="radio" className="modal__radio-input" /> Warm
+            <input
+              id="warm"
+              type="radio"
+              className="modal__radio-input"
+              name="weatherType"
+              value="Warm"
+            />
+            Warm
           </label>
           <label
             htmlFor="cold"
             className="modal__label modal__label_type_radio"
           >
-            <input id="cold" type="radio" className="modal__radio-input" /> Cold
+            <input
+              id="cold"
+              type="radio"
+              className="modal__radio-input"
+              name="weatherType"
+              value="Cold"
+            />
+            Cold
           </label>
         </fieldset>
       </ModalWithForm>
