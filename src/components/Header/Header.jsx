a883c5ecbe7a6cd490df03fc6dac2971a713a6c0
Kyle Le <158103657+Kyle-Le-create@users.noreply.github.com>
2024-09-11 14:46:31 -0500

    New update for my project

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,6 +2,7 @@ import "./Header.css";
 import logo from "../../assets/logos.svg";
 import avatar from "../../assets/avatar.svg";
 import ToggleSwitch from "../ToggleSwitch/ToggleSwitch";
+import { Link } from "react-router-dom";
 
 function Header({
   handleAddClick,
@@ -16,7 +17,9 @@ function Header({
 
   return (
     <header className="header">
-      <img className="header__logo" src={logo} alt="Logo" />
+      <Link to="/">
+        <img className="header__logo" src={logo} alt="Logo" />
+      </Link>
       <p className="header__date-and-location">
         {currentDate}, {weatherData.city}
       </p>
@@ -31,10 +34,12 @@ function Header({
       >
         + ADD CLOTHES
       </button>
-      <div className="header__user-container">
-        <p className="header__username">Terrence Tegegne</p>
-        <img src={avatar} alt="Terrence Tegegne" className="header__avatar" />
-      </div>
+      <Link to="/profile" className="header__link">
+        <div className="header__user-container">
+          <p className="header__username">Terrence Tegegne</p>
+          <img src={avatar} alt="Terrence Tegegne" className="header__avatar" />
+        </div>
+      </Link>
     </header>
   );
 }
