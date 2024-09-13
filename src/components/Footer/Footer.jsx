18106f70d7a776e51a98f4b479a472a33d2d0739
Kyle Le <158103657+Kyle-Le-create@users.noreply.github.com>
2024-09-12 19:53:15 -0500

    New update for my project

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <footer>
       <span className="footer">
-        <p className="footer__text">Developed by Tia Giron</p>
+        <p className="footer__text">Developed by Kyle Le</p>
         <p className="footer__text">2024</p>
       </span>
     </footer>
