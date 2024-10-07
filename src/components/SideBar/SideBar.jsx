ee9d52e3334724ce0ad3bf2d39290eb4f1f0b623
Kyle Le <158103657+Kyle-Le-create@users.noreply.github.com>
2024-10-07 15:51:04 -0500

    New update on my project 14

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,13 +1,39 @@
 import "./SideBar.css";
 import avatar from "../../assets/avatar.svg";
+import CurrentUserContext from "../../contexts/CurrentUserContext";
+import { useContext } from "react";
 
-function SideBar() {
+function SideBar({ handleLogOutClick, handleEditProfileClick }) {
+  const currentUser = useContext(CurrentUserContext);
   return (
     <div className="sidebar">
-      <img src={avatar} alt="Avatar" className="sidebar__avatar" />
-      <p className="sidebar__username">Kyle Le</p>
+      <div className="sidebar__profile-info">
+        <img
+          className="sidebar__avatar"
+          src={currentUser?.avatar}
+          alt="profile image"
+        />
+        <p className="sidebar__username">{currentUser?.name}</p>
+      </div>
+      <div className="sidebar__buttons">
+        <button
+          className="sidebar__button"
+          type="button"
+          onClick={handleEditProfileClick}
+        >
+          {" "}
+          Change Profile Data
+        </button>
+        <button
+          className="sidebar__button"
+          type="button"
+          onClick={handleLogOutClick}
+        >
+          {" "}
+          Log Out
+        </button>
+      </div>
     </div>
   );
 }
-
 export default SideBar;
